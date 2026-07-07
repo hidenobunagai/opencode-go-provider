53ad2a3c150ecf7a731c016b69bac91b0b43cb60
Hidenobu Nagai <hidenobu.nagai.jp@gmail.com>
2026-07-07 23:11:23 +0900

    Phase 3A: Add coverage measurement with 50% threshold and CI artifact upload

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,16 @@ module.exports = {
   testMatch: ["**/*.test.ts"],
   moduleFileExtensions: ["ts", "js"],
   collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  coverageThreshold: {
+    global: {
+      lines: 50,
+      branches: 50,
+      functions: 50,
+      statements: 50,
+    },
+  },
   moduleNameMapper: {
     "^vscode$": "<rootDir>/__mocks__/vscode.ts",
     "^../package.json$": "<rootDir>/package.json",
